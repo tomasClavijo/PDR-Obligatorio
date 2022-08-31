71a6c9976aaf45ca69771ba227db2650d7270ffe
tomasClavijo <tmsclvj410@gmail.com>
2022-08-31 16:18:53 -0300

    Creación de Perfil

---
+++ PRedes/ConsoleApp1/Perfil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace LKdin
+{
+    public class Perfil : Usuario
+    {
+        List<String> habilidades { get; set; }
+
+        String descripcion { get; set; }
+
+        Bitmap imagen { get; set; }
+
+    }
+}
