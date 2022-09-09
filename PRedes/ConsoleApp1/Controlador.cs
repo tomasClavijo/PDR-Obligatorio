b53ab31852eef80b0084a780649032468403b36c
tomasClavijo <tmsclvj410@gmail.com>
2022-09-09 17:42:37 -0300

    Buscar Usuarios

--- PRedes/ConsoleApp1/Controlador.cs
+++ PRedes/ConsoleApp1/Controlador.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,10 +26,27 @@ namespace LKdin
 
         public Usuario BuscarUsuarioId(String idUsuario)
         {
-            return null;
+            Usuario usuario = new Usuario();
+            usuario.UserId = idUsuario;
+            for (int i = 0; i < Usuarios.Count; i++)
+            {
+                if (Usuarios[i].UserId.Equals(usuario.UserId))
+                {
+                    return Usuarios[i];
+                }
+            }
         }
+
         public List<Usuario> BuscarUsuarioNombre(String Nombre){
-            return null;
+            Usuario usuario = new Usuario();
+            usuario.Name = Nombre;
+            for (int i = 0; i < Usuario.Count; i++)
+            {
+                if (Usuario[i].Name.Equals(usuario.Name))
+                {
+                    return Usuarios[i];
+                }
+            }
         }
 
         public List<Usuario> BuscarPorHabilidad(List<String> habilidades)
